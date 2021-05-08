c370fab5010e68e0f685a9aef0da37f42f82182d
Tim <qwertyqwer.teamhavok@gmail.com>
2021-05-08 21:23:01 +1200

    for rugs

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Hello World</h1>
+  <Search />
+</template>
+
+<script>
+
+const color = '#333333';
+
+import Search from './components/Search'
+
+export default {
+  name: 'App',
+  components: {
+    Search,
+  },
+}
+</script>
+
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <input type="text" name="searchBox" placeholder="Search here...">
+  <h1>nibba</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
